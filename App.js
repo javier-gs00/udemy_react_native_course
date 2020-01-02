e9e7aa9ef4d869fa021db56674914dfe118d0e79
Javier Gutierrez <javier.guse@icloud.com>
2020-01-01 21:47:13 -0300

    reset template

--- App.js
+++ App.js
@@ -1,65 +1,17 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, FlatList, Button } from 'react-native';
-import { GoalItem } from './components/GoalItem';
-import { GoalInput } from './components/GoalInput';
 
 export default function App() {
-  const [courseGoals, setCourseGoal] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
-
-  const addGoalHandler = goalTitle => {
-    // This guarantees that the latest state is being used
-    setCourseGoal(currentGoals => [
-      ...currentGoals,
-      // The 'key' is necessary when using FlatList ('id' is also valid)
-      // others property keys can be used in conjunction with keyExtractor
-      { _id: Math.random().toString(), value: goalTitle }
-    ]);
-    setIsAddMode(false);
-  };
-
-  const removeGoalHandler = goalId => {
-    setCourseGoal(currentGoals => {
-      return currentGoals.filter(goal => goal._id !== goalId);
-    });
-  };
-
-  const cancelGoalAdditionHandler = () => {
-    setIsAddMode(false);
-  };
-
   return (
-    <View style={styles.screen}>
-      <Button title="Add new Goal" onPress={() => setIsAddMode(true)} />
-      <GoalInput
-        visible={isAddMode}
-        onAddGoal={addGoalHandler}
-        onCancel={cancelGoalAdditionHandler}
-      />
-      <FlatList
-        keyExtractor={(item, index) => item._id}
-        data={courseGoals}
-        renderItem={itemData => (
-          <GoalItem
-            id={itemData.item._id}
-            title={itemData.item.value}
-            onDelete={removeGoalHandler}
-          />
-        )}
-      />
-      {/* <ScrollView>
-        {courseGoals.map((goal, index) => (
-          <View key={index} style={styles.listItem}>
-            <Text>{goal}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+    <View style={styles.container}>
+      <Text>Starting template</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
   }
 });
