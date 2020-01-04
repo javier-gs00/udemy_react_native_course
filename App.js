62939b3577c37fe91833508ce06b073626e17b7a
Javier Gutierrez <javier.guse@icloud.com>
2020-01-04 17:21:38 -0300

    Create basic components and screens

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Starting template</Text>
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
+  screen: {
+    flex: 1
   }
 });
---
+++ components/Card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = props => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    // only for ios
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    // only for android
+    elevation: 5
+  }
+});
+
+export default Card;
